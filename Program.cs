6c8036cdc91c5e0f5ae8c1e41097b7e8dca09564
Menna Alaa <menaalaa460@gmail.com>
2026-09-06 05:34:27 +0300

    Q1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿#region Q1
+//Create a one - dimensional array double[] prices with the values 25.5, 40.0, 33.75. Print 
+//the second price (index 1).
+
+//double[] prices = { 25.5, 40.0, 33.75 };
+
+//Console.WriteLine(prices[1]);
+#endregion
